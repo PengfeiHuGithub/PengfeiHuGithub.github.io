ef58afbebac26db145225687aff8c47369973843
hu_peng_fei_8 <724256598@qq.com>
2020-08-13 11:48:37 +0800

    a

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    base: '',
+    title: '我的个人博客',
+    dest: './production',
+    markdown: {
+      lineNumbers: true
+    },
+    /*配置文件*/
+    themeConfig: {
+      /*横向导航*/
+      nav: require('../guide/nav'), // 1.先配置横向导航
+      sidebar: require('../guide/sidebar'),//2.
+      sidebarDepth: 2,
+      lastUpdated: 'Last Updated',
+      searchMaxSuggestions: 10,
+      serviceWorker: {
+        updatePopup: {
+          message: "有新的内容.",
+          buttonText: '更新'
+        }
+      }
+    }
+  };
---
+++ docs/guide/nav.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+      text: '前端面试', link: '/web/',
+      items: [
+        {text: '优化相关之webpack', link: '/web/webpack/'},
+        {text: 'git', link: '/web/git/'},
+      ]
+    },
+    {
+      text: '后端面试', link: '/web/',
+      items: []
+    },
+  
+  ];
