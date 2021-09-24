be207040c2f61af203c40c52f246c1bf7ec742a8
v-hupengfei <v-hupengfei@xiaomi.com>
2021-09-24 13:38:52 +0800

    feat:更新了一些文章

--- docs/guide/nav.js
+++ docs/guide/nav.js
@@ -1,54 +1,132 @@
-module.exports = [
-    {
-        text: '基础', link: '/web-basics/'
+module.exports = [{
+        text: '进阶',
+        link: '/web-advanced/'
     },
+    // {
+    //     text: '基础',
+    //     link: '/web-foundation/',
+    //     items: [{
+    //             text: 'HTML、HTTP、web综合问题',
+    //             link: '/web-foundation/html_foundation/'
+    //         },
+    //         {
+    //             text: 'CSS部分',
+    //             link: '/web-foundation/css_part/'
+    //         },
+    //         {
+    //             text: 'JavaScript部分',
+    //             link: '/web-foundation/javascript_part/'
+    //         },
+    //         {
+    //             text: 'jQuery部分',
+    //             link: '/web-foundation/jquery_part/'
+    //         },
+    //         {
+    //             text: 'BootStrap部分',
+    //             link: '/web-foundation/bootstrap_part/'
+    //         },
+    //         {
+    //             text: '微信小程序部分',
+    //             link: '/web-foundation/wxapplets_part/'
+    //         },
+    //         {
+    //             text: 'Webpack部分',
+    //             link: '/web-foundation/webpack_part/'
+    //         },
+    //         {
+    //             text: '编程题部分',
+    //             link: '/web-foundation/program_part/'
+    //         },
+    //         {
+    //             text: '其他',
+    //             link: '/web-foundation/others_part/'
+    //         },
+    //         {
+    //             text: '综合',
+    //             link: '/web-foundation/comprehensive_part/'
+    //         },
+    //         {
+    //             text: '常见问题',
+    //             link: '/web-foundation/question_part/'
+    //         },
+    //         {
+    //             text: 'HR部分',
+    //             link: '/web-foundation/hr_part/'
+    //         }
+    //     ]
+    // },
     {
-        text: '进阶', link:'/web-advanced/'
-    },
-    {
-        text: '精选', link:'/web-featured/',
-        items: [
-            { text: 'HTML模块', link:'/web-featured/html/'},
-            { text: 'CSS模块', link:'/web-featured/css/'},
-            { text: 'JS模块儿', link:'/web-featured/js/'}
+        text: '精选',
+        link: '/web-featured/',
+        items: [{
+                text: 'HTML模块',
+                link: '/web-featured/html/'
+            },
+            {
+                text: 'CSS模块',
+                link: '/web-featured/css/'
+            },
+            {
+                text: 'JS模块儿',
+                link: '/web-featured/js/'
+            }
         ]
     },
     {
-        text: '高频', link: '/web-high_frequency/'
+        text: '高频',
+        link: '/web-high_frequency/'
     },
     {
-        text: '指南', link: '/web-guide/'
+        text: '指南',
+        link: '/web-guide/'
     },
     {
-        text: '性能', link: '/web-performance/'
+        text: '性能',
+        link: '/web-performance/'
     },
     {
-        text: '综合', link: '/web-comprehensive/'
+        text: '综合',
+        link: '/web-comprehensive/'
     },
     {
-        text: '原理', link: '/web-principle/'
+        text: '原理',
+        link: '/web-principle/'
     },
     {
-        text: '手写', link: '/web-handwriting/'
+        text: '手写',
+        link: '/web-handwriting/'
     },
     {
-        text: '模式', link: '/web-model/'
+        text: '模式',
+        link: '/web-model/'
     },
     {
-        text: '算法', link: '/web-algorithm/'
+        text: '算法',
+        link: '/web-algorithm/'
     },
     {
-        text: '前端面试', link: '/web/',
-        items: [
-            { text: '优化相关之webpack', link: '/web/webpack/' },
-            { text: 'git', link: '/web/git/' },
-            { text: 'vue知识点解读', link: '/web/vue难点解读/'},
+        text: '前端面试',
+        link: '/web/',
+        items: [{
+                text: '优化相关之webpack',
+                link: '/web/webpack/'
+            },
+            {
+                text: 'git',
+                link: '/web/git/'
+            },
+            {
+                text: 'vue知识点解读',
+                link: '/web/vue难点解读/'
+            },
         ]
     },
     {
-        text: '拓展阅读', link: '/web-expand/',
-        items: [
-            { text: '浏览器原理', link: '/web-expand/browser/'}
-        ]
+        text: '拓展阅读',
+        link: '/web-expand/',
+        items: [{
+            text: '浏览器原理',
+            link: '/web-expand/browser/'
+        }]
     }
 ];
