692df9373e3e3aab5e1dd3d8f4338888e49b3c84
hpf <pengfei.hu@dxdstech.com>
2026-08-27 15:04:48 +0800

    ci: add github action for auto deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     head: [ // 注入到当前页面的 HTML <head> 中的标签
         ['link', { rel: 'icon', href: '/logo3.png' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
-    dest: './production',
+    dest: 'docs/.vuepress/dist',
     markdown: {
         lineNumbers: true
     },
